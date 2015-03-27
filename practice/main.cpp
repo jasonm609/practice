e4590d78bfa1544f57541aa48061b6c9b32f87e0
Jason Mercurio <jmercuri@ucsd.edu>
2015-03-27 12:57:33 -0700

    test

--- practice/main.cpp
+++ practice/main.cpp
@@ -9,7 +9,16 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
+    
     // insert code here...
     std::cout << "Hello, World!\n";
+    std::cout << "Test" << std::endl;
+    
+    std::string string1;
+    std::cin >> string1;
+    
+    std::cout << string1 << std::endl;
+    //std::cout << string1[0] << std::endl;
+    
     return 0;
 }
