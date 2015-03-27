0d02a843da191dd8a0edb0e8f032237db0b79154
Jason Mercurio <jmercuri@ucsd.edu>
2015-03-27 12:40:54 -0700

    Initial Commit

---
+++ practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by Jason Mercurio on 3/27/15.
+//  Copyright (c) 2015 Jason Mercurio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
